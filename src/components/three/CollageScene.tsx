756029eae8c2fb8e64a6f7764ab4da969cf6a6f3
fusioneventsak <arthurk@fusion-events.ca>
2025-05-28 10:55:52 -0400

    Fix grid and floor rendering order

--- src/components/three/CollageScene.tsx
+++ src/components/three/CollageScene.tsx
@@ -530,10 +530,25 @@ const Floor: React.FC<{ settings: SceneSettings }> = ({ settings }) => {
   if (!settings.floorEnabled) return null;
 
   return (
-    <group>
+    <group renderOrder={1}>
+      {settings.gridEnabled && isGridReady && (
+        <Grid
+          position={[0, -2, 0]}
+          args={[settings.gridSize, settings.gridDivisions]}
+          cellSize={1}
+          cellThickness={0.5}
+          cellColor={settings.gridColor}
+          sectionSize={3}
+          fadeDistance={30}
+          fadeStrength={1}
+          followCamera={false}
+          infiniteGrid={false}
+        />
+      )}
       <mesh
         rotation={[-Math.PI / 2, 0, 0]}
         position={[0, -2.001, 0]}
+        renderOrder={0}
       >
         <planeGeometry args={[settings.floorSize, settings.floorSize]} />
         <meshStandardMaterial
@@ -546,21 +561,6 @@ const Floor: React.FC<{ settings: SceneSettings }> = ({ settings }) => {
           side={THREE.DoubleSide}
         />
       </mesh>
-      {settings.gridEnabled && isGridReady && (
-        <Grid
-          position={[0, -1.999, 0]}
-          args={[settings.gridSize, settings.gridDivisions]}
-          cellSize={1}
-          cellThickness={0.5}
-          cellColor={settings.gridColor}
-          sectionSize={3}
-          fadeDistance={50}
-          fadeStrength={2}
-          followCamera={false}
-          infiniteGrid={false}
-          renderOrder={1}
-        />
-      )}
     </group>
   );
 };
