ab7e044686c9b27246668da2df5c5156bb59fa42
fusioneventsak <arthurk@fusion-events.ca>
2025-05-28 11:04:17 -0400

    Fix grid rendering above floor

--- src/components/three/CollageScene.tsx
+++ src/components/three/CollageScene.tsx
@@ -533,10 +533,10 @@ const Floor: React.FC<{ settings: SceneSettings }> = ({ settings }) => {
   return (
     <>
       <mesh
-        ref={floorRef}
-        rotation={[-Math.PI / 2, 0, 0]}
-        position={[0, -2.001, 0]}
-        receiveShadow
+        ref={floorRef} 
+        rotation={[-Math.PI / 2, 0, 0]} 
+        position={[0, -2, 0]} 
+        receiveShadow 
       >
         <planeGeometry args={[settings.floorSize, settings.floorSize]} />
         <meshStandardMaterial
@@ -551,6 +551,7 @@ const Floor: React.FC<{ settings: SceneSettings }> = ({ settings }) => {
       {settings.gridEnabled && isGridReady && (
         <Grid
           position={[0, -2, 0]}
+          renderOrder={1}
           args={[settings.gridSize, settings.gridDivisions]}
           cellSize={1}
           cellThickness={0.5}
