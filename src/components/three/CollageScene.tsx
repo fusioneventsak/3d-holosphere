58d7d75ca86b619f7f17d8b48f8f8a6eeb8cbda6
fusioneventsak <arthurk@fusion-events.ca>
2025-05-29 13:17:40 -0400

    Fix floor grid orientation

--- src/components/three/CollageScene.tsx
+++ src/components/three/CollageScene.tsx
@@ -234,20 +234,20 @@ const Floor: React.FC<{ settings: SceneSettings }> = ({ settings }) => {
         />
       </mesh>
       
-      {/* Grid overlay - positioned slightly above the floor to prevent z-fighting */}
+      {/* Grid overlay - positioned slightly above the floor */}
       {settings.gridEnabled && (
-        <mesh position={[0, 0.01, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-          <Grid
-            args={[settings.floorSize, settings.floorSize]}
-            cellSize={1}
-            cellThickness={0.5}
-            cellColor={settings.gridColor}
-            sectionSize={Math.ceil(settings.gridDivisions / 10)}
-            fadeDistance={30}
-            fadeStrength={1}
-            infiniteGrid={false}
-          />
-        </mesh>
+        <Grid
+          position={[0, 0.01, 0]}
+          rotation={[-Math.PI / 2, 0, 0]}
+          args={[settings.floorSize, settings.floorSize]}
+          cellSize={1}
+          cellThickness={0.5}
+          cellColor={settings.gridColor}
+          sectionSize={Math.ceil(settings.gridDivisions / 10)}
+          fadeDistance={30}
+          fadeStrength={1}
+          infiniteGrid={false}
+        />
       )}
     </group>
   );
