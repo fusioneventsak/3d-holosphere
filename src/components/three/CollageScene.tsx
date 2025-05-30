516cd3a5a78d86317cb74215c7fb99736d34f5b0
fusioneventsak <arthurk@fusion-events.ca>
2025-05-30 16:34:48 -0400

    Fix Grid component syntax

--- src/components/three/CollageScene.tsx
+++ src/components/three/CollageScene.tsx
@@ -406,16 +406,16 @@ const Floor: React.FC<{ settings: SceneSettings }> = ({ settings }) => {
       {/* Grid overlay - positioned slightly above the floor */}
       {settings.gridEnabled && (
         <Grid
-          position={[0, 0.01, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          args={[settings.floorSize, settings.floorSize]}
-          cellSize={1}
-          cellThickness={0.5}
-          cellColor={settings.gridColor}
-          sectionSize={Math.ceil(settings.gridDivisions / 10)}
-          fadeDistance={30}
-          fadeStrength={1}
-          infiniteGrid={false}
+          position={[0, 0.01, 0]} 
+          rotation={[-Math.PI / 2, 0, 0]} 
+          args={[settings.floorSize, settings.floorSize]} 
+          cellSize={1} 
+          cellThickness={0.5} 
+          cellColor={settings.gridColor} 
+          sectionSize={Math.ceil(settings.gridDivisions / 10)} 
+          fadeDistance={30} 
+          fadeStrength={1} 
+          infiniteGrid={false} 
         />
       )}
     </group>
