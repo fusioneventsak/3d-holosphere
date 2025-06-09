5cb97c3700a63f9c17f7f194ca105a0bc204253f
fusioneventsak <arthurk@fusion-events.ca>
2025-06-08 21:38:32 -0400

    Fix DemoRequestModal import path

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CameraIcon, Users } from 'lucide-react';
-import DemoRequestModal from '../DemoRequestModal';
+import DemoRequestModal from '../modals/DemoRequestModal';
 
 const Header: React.FC = () => {
   const [isDemoModalOpen, setIsDemoModalOpen] = useState(false);
